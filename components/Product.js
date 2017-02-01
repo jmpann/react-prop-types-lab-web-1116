5472109eb215dc4bacd5097c352db1c4dba9c9c9
Josh Pann <josh.pann@example.com>
2017-02-01 17:02:08 -0500

    Done.

--- components/Product.js
+++ components/Product.js
@@ -31,13 +31,13 @@ Product.propTypes = {
 
   function weightValidation(props, propName, compenentName){
     if(props[propName] === undefined){
-      return new Error('YOU NEED TO PUT THIS IN. WHY YOU SO STUPID??? PEOPLE LIKE YOU MAKE MY JOB SO MUCH HARDER')
+      return new Error('This is required')
     }
     else if (isNaN(props[propName])){
-      return new Error( 'IT SAYS NUMBER! WHY DID YOU PUT IN TEXT? L2 READ DUMBASS.')
+      return new Error( 'This must be a number')
     }
     else if (props[propName] < 80 || props[propName] > 300){
-      return new Error('you are fatter than patty.')
+      return new Error('please enter a weight within the specified range')
   }
   return null;
 }
